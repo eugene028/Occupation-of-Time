8c9abc445f6e5d850d76c4ee5cb231f377dc74c4
Eugene Kim <eugene02876@gmail.com>
2023-05-01 03:34:41 +0900

    시공간의 점유

---
+++ script.js
@@ -0,0 +1,34 @@
+var mainbox = document.querySelector(".view");
+mainbox.addEventListener('click', function(){
+        mainbox.classList.remove("hidden");
+        console.log("클릭");
+});
+
+var youtube = document.querySelector(".youtube");
+youtube.addEventListener('click', function(){
+    window.open('https://youtu.be/emq3ZVNCUGs');
+})
+
+var formbutton = document.querySelector(".formdata");
+formbutton.addEventListener('click',function(){
+    document.querySelector('.form-container').scrollIntoView({behavior:'smooth'});
+})
+
+var sidebar = document.querySelector(".sidebarCollapse");
+sidebar.addEventListener('click',function(){
+    document.querySelector(".sidebar").classList.add("active");
+    document.querySelector(".overlay").style.display ="block";
+})
+
+var overlay = document.querySelector(".overlay");
+overlay.addEventListener('click',function(){
+    document.querySelector(".sidebar").classList.remove("active");
+    document.querySelector(".overlay").style.display ="none";
+})
+
+var sidebutton = document.querySelector(".sideform");
+sidebutton.addEventListener('click',function(){
+    document.querySelector(".sidebar").classList.remove("active");
+    document.querySelector(".overlay").style.display ="none";
+    document.querySelector('.form-container').scrollIntoView({behavior:'smooth'});
+});
